88cdae8ef54d88c9c3bc3a07e20f1de0237c1faa
倉徳 大嶺 <omine@implem.co.jp>
2020-07-18 11:12:49 +0900

    エントリーファイルの修正

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,8 +6,7 @@ module.exports = {
     ignored: /node_modules/
   },
   entry: {
-    index: ['@babel/polyfill', './src/index.js'],
-    edit: ['@babel/polyfill', './src/edit.js']
+    index: ['@babel/polyfill', './src/index.js']
   },
   output: {
     path: __dirname + '/dist',
@@ -16,17 +15,11 @@ module.exports = {
   module: {
     rules: [
       {
-        // ローダーの処理対象ファイル
         test: /\.js$/,
-        // ローダーの処理対象から外すディレクトリ
         exclude: /node_modules/,
         use: [
           {
-            // 利用するローダー
             loader: 'babel-loader',
-            // ローダーのオプション
-            // 今回はbabel-loaderを利用しているため
-            // babelのオプションを指定しているという認識で問題ない
             options: {
               presets: [['@babel/preset-env', { modules: false }]]
             }
--- webpack.pro.config.js
+++ webpack.pro.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   mode: 'production',
   entry: {
-    index: ['@babel/polyfill', './src/index.js'],
-    edit: ['@babel/polyfill', './src/edit.js']
+    index: ['@babel/polyfill', './src/index.js']
   },
   output: {
     path: __dirname + '/dist',
@@ -11,17 +10,11 @@ module.exports = {
   module: {
     rules: [
       {
-        // ローダーの処理対象ファイル
         test: /\.js$/,
-        // ローダーの処理対象から外すディレクトリ
         exclude: /node_modules/,
         use: [
           {
-            // 利用するローダー
             loader: 'babel-loader',
-            // ローダーのオプション
-            // 今回はbabel-loaderを利用しているため
-            // babelのオプションを指定しているという認識で問題ない
             options: {
               presets: [['@babel/preset-env', { modules: false }]]
             }
